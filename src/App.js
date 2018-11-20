ddf219d11133815eb975eef56cac9b7664c5a131
Sophia Mai <phuongmai001@gmail.com>
2018-11-19 20:37:55 -0800

    feat: create a provider to manage all states

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import './App.css'
 import Search from './view/Search'
 import Home from './view/Home'
 import { Switch, Route } from 'react-router-dom'
+import Provider, { MyContext } from './Provider'
 
 class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <Switch>
-          <Route exact path={"/"} component={Home} />
-          <Route exact path={"/search"} component={Search} />
-        </Switch>
+        <Provider>
+          <Switch>
+            <Route exact path={"/"}
+              render={() => (
+                <MyContext.Consumer>
+                  { context => <Home { ...context } />}
+                </MyContext.Consumer>
+              )} />
+            <Route exact path={"/search"} component={Search} />
+          </Switch>
+        </Provider>
       </div>
     )
   }
