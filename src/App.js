9d46870a4bdf7ac99958953b2e338969c423d394
Sophia Mai <phuongmai001@gmail.com>
2018-11-19 20:02:40 -0800

    feat: add home and search page routing

--- src/App.js
+++ src/App.js
@@ -3,26 +3,16 @@ import React from 'react'
 import './App.css'
 import Search from './view/Search'
 import Home from './view/Home'
+import { Switch, Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <Search />
-        ) : (
-          <Home />
-        )}
+        <Switch>
+          <Route exact path={"/"} component={Home} />
+          <Route exact path={"/search"} component={Search} />
+        </Switch>
       </div>
     )
   }
